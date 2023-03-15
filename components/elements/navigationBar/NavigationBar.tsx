73c9d8b3e2037b3328d8b329bf2c7192602fb621
Anthony Urbina <aurbinava002@gmail.com>
2023-03-15 02:19:06 +0000

    user can connect and disconnect wallet

--- components/elements/navigationBar/NavigationBar.tsx
+++ components/elements/navigationBar/NavigationBar.tsx
@@ -14,64 +14,37 @@ import {
     MenuItem,
 } from '@chakra-ui/react'
 // import { GiHamburgerMenu } from 'react-icons/gi'
-import { useRouter } from 'next/router'
-import NextLink from 'next/link'
-import * as routes from '../../../constants/routes'
+import { useRouter } from 'next/router';
+import { WagmiConfig, createClient } from "wagmi";
+import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
+import NextLink from 'next/link';
+import * as routes from '../../../constants/routes';
+
+const alchemyId = 'EoOTjHvOIujYqlStV6ppG71Y7Cf94wRj';
+
+const client = createClient(
+    getDefaultClient({
+        appName: "ROFL",
+        alchemyId,
+    }),
+);
 
 export const NavigationBar = () => {
     const router = useRouter()
 
     return (
-        <Flex
-            position="absolute"
-            top={0}
-            justifyContent={['initial', null, 'center']}
-            width="100%"
-            opacity={0.99}
-        >
-            <Box
-                position="absolute"
-                height="100%"
-                width="100%"
-                bgGradient="linear(to-r, backgroundGradientPrimary, backgroundGradientSecondary)"
-                filter="blur(24px)"
-                opacity={0.99}
-            />
-
-            <Flex
-                justifyContent="center"
-                width="100%"
-                backgroundColor="white"
-                opacity={0.99}
-            >
-                <Flex
-                    flexDirection="row"
-                    justifyContent="space-between"
-                    alignItems="center"
-                    height={['100px', null, '160px']}
-                    width={['100%', null, null, null, '80em']}
-                    paddingLeft={4}
-                    paddingRight={4}
-                    marginLeft={[0, null, 4]}
-                    marginRight={[0, null, 4]}
-                >
-
-                    <Flex display={['flex', null, null, null, 'none']}>
-                        <Menu >
-                            <MenuButton
-                                as={IconButton}
-                                // aria-label='Options'
-                                // icon={<GiHamburgerMenu />}
-                                variant='outline'
-                            >
-
-                            </MenuButton>
-                            <MenuList marginTop={3} pl={10} minW="0" borderRadius={'25px'} w={'255px'}>
-                            </MenuList>
-                        </Menu>
-                    </Flex>
-                </Flex>
-            </Flex>
-        </Flex >
+        <Flex justifyContent="space-between" alignItems="center" borderBottom="1px">
+            <Box paddingX={['.5rem', null, '1.5rem']} paddingY='.5rem'>
+                <Image boxSize='3rem' src='/static/rofl-logo.png' alt='rofl-logo.png'/>
+            </Box>
+            <Box paddingLeft={['.5rem', null, '1.5rem']} paddingY='.5rem'>
+                <WagmiConfig client={client}>
+                    <ConnectKitProvider theme='rounded'>
+                        <ConnectKitButton />
+                    </ConnectKitProvider>
+                </WagmiConfig>
+            </Box>
+        </Flex>
     )
+
 }
