ea23e6728aacac0ecf339370601a3bee6830c070
Anthony Urbina <aurbinava002@gmail.com>
2023-03-15 20:28:08 +0000

    create raffle button in place

--- components/elements/navigationBar/NavigationBar.tsx
+++ components/elements/navigationBar/NavigationBar.tsx
@@ -12,7 +12,7 @@ import {
     MenuButton,
     MenuList,
     MenuItem,
-    Icon
+    Text
 } from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
 // import { GiHamburgerMenu } from 'react-icons/gi'
@@ -35,20 +35,28 @@ export const NavigationBar = () => {
     const router = useRouter()
 
     return (
-        <Flex justifyContent="space-between" alignItems="center" borderBottom="1px">
-            <Box paddingLeft={['.5rem', null, '1.5rem']} paddingY='.5rem'>
-                <Image boxSize='3rem' src='/static/rofl-logo.png' alt='rofl-logo.png'/>
-            </Box>
-            <Flex borderRadius='.25rem' boxShadow='shadows.grey'>
-                <AddIcon m='.5rem' boxSize={5}/>
-            </Flex>
-            <Box paddingRight={['.5rem', null, '1.5rem']} paddingY='.5rem'>
+        <Flex justify="space-between" align="center" borderBottom="1px">
+            <Link as={NextLink} href='' _activeLink={{ textDecor: 'none' }} _hover={{ textDecor: 'none' }}>
+                <Flex alignItems="center" paddingX={['.5rem', null, '1.5rem']} paddingY='.5rem'>
+                    <Box>
+                        <Image boxSize='3rem' src='/static/rofl-logo.png' alt='rofl-logo.png' />
+                    </Box>
+                    <Box>
+                        <Text fontFamily='Syne Tactile' fontSize='1.25rem' color='#C95CC9' ml='.5rem'>ROFL</Text>
+                    </Box>
+                </Flex>
+            </Link>
+
+            <Flex>
+                <IconButton bgColor='white' aria-label='create raffle' p='1.25rem' m='.5rem' boxSize={5} icon={<AddIcon />} boxShadow="inset 0 0 0 2px #DFE4EC,0 2px 0 0 #DFE4EC,0px 2px 4px rgba(0,0,0,0.02);" />
+                <Box paddingRight={['.5rem', null, '1.5rem']} paddingY='.5rem'>
                     <WagmiConfig client={client}>
                         <ConnectKitProvider theme='rounded'>
                             <ConnectKitButton />
                         </ConnectKitProvider>
                     </WagmiConfig>
-             </Box>
+                </Box>
+            </Flex>
         </Flex>
     )
 
