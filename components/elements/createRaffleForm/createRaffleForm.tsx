ffc9f9c849ab707975781f615e4fbd877f214055
Anthony Urbina <aurbinava002@gmail.com>
2023-03-16 06:39:40 +0000

    saving progress. input validation. deciding whether to do validation for indiv boxes or 1 validation at a time at the bottom of the form

--- components/elements/createRaffleForm/createRaffleForm.tsx
+++ components/elements/createRaffleForm/createRaffleForm.tsx
@@ -24,7 +24,7 @@ import {
     Select
 } from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 export interface CreateRaffleFormProps {
     title: string
@@ -45,7 +45,39 @@ export const CreateRaffleForm = ({
     route,
     past
 }: CreateRaffleFormProps) => {
-    const [currency, setCurrency] = useState('ETH')
+    const [ticketPrice, setTicketPrice] = useState('')
+    const [reservePrice, setReservePrice] = useState('')
+
+    const [maxPrecisionTicketErr, setMaxPrecisionTicketErr] = useState(false)
+    const [maxPrecisionReserveErr, setMaxPrecisionReserveErr] = useState(false)
+
+
+    const handleTicketPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        const parsedValue = parseFloat(value);
+        const MAX_DECIMAL_PLACES = 18
+        setTicketPrice(value);
+        if (parsedValue && value.includes('.') && value.split('.')[1].length > MAX_DECIMAL_PLACES) {
+            setMaxPrecisionTicketErr(true)
+        } else {
+            setMaxPrecisionTicketErr(false)
+
+        }
+    };
+
+    const handleReservePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        const parsedValue = parseFloat(value);
+        const MAX_DECIMAL_PLACES = 18
+        setReservePrice(value);
+        if (parsedValue && value.includes('.') && value.split('.')[1].length > MAX_DECIMAL_PLACES) {
+            setMaxPrecisionReserveErr(true)
+        } else {
+            setMaxPrecisionReserveErr(false)
+
+        }
+    };
+
     return (
         <>
     <Flex border='1px' rounded='1rem' align='center' justify='center' boxSize={353} m='0 auto' bgColor='#D9D9D9'>
@@ -55,7 +87,7 @@ export const CreateRaffleForm = ({
             </Button>
         </Box>
     </Flex>
-        <FormControl >
+        <FormControl>
             <Flex p='1rem' flexWrap='wrap' justify='space-between' rounded='1rem' border='1px' m='.75rem'>
                 <Flex direction='column' align='center'>
                     <FormLabel marginEnd='none' mb='none'>Currency</FormLabel>
@@ -65,19 +97,31 @@ export const CreateRaffleForm = ({
                     </Select>
                 </Flex>
                 <Flex direction='column' align='center'>
-                    <FormLabel marginEnd='none' mb='none' >Ticket Price</FormLabel>
-                    <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
-                    <Input type='number' w='150px' h='25px' rounded='1rem' borderColor='black'></Input>
+                    <FormControl isInvalid={maxPrecisionTicketErr}>
+                        <FormLabel marginEnd='none' mb='none' textAlign='center'>Ticket Price</FormLabel>
+                        {
+                            maxPrecisionTicketErr ?
+                                <FormErrorMessage mt='none' mb='.25rem' fontSize='9px' textAlign='center'>18 decimal points max</FormErrorMessage>
+                                : <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
+                        }
+                        <Input data-view-field='ticket-price' type='text' w='150px' h='25px' rounded='1rem' borderColor='black' value={ticketPrice} onChange={handleTicketPriceChange}></Input>
+                    </FormControl>
                 </Flex>
                 <Flex direction='column' align='center' pt='1rem'>
-                    <FormLabel marginEnd='none' mb='none'>Reserve Price</FormLabel>
-                    <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Raffle minimum</FormHelperText>
-                    <Input type='number' w='150px' h='25px' rounded='1rem' borderColor='black'></Input>
+                    <FormControl isInvalid={maxPrecisionReserveErr}>
+                        <FormLabel marginEnd='none' mb='none'>Reserve Price</FormLabel>
+                        {
+                            maxPrecisionReserveErr ?
+                                <FormErrorMessage mt='none' mb='.25rem' fontSize='9px' textAlign='center'>18 decimal points max</FormErrorMessage>
+                                : <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
+                        }
+                        <Input type='number' w='150px' h='25px' rounded='1rem' borderColor='black' value={reservePrice} onChange={handleReservePriceChange}></Input>
+                    </FormControl>
                 </Flex>
                 <Flex direction='column' align='center' pt='1rem'>
                     <FormLabel marginEnd='none' mb='none'>Raffle End Date</FormLabel>
                     <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Raffle minimum</FormHelperText>
-                    <Input type='number' w='150px' h='25px' rounded='1rem' borderColor='black'></Input>
+                    <Input type='text' w='150px' h='25px' rounded='1rem' borderColor='black'></Input>
                 </Flex>
             </Flex>
             <Flex justify='center' pb='.5rem'>
