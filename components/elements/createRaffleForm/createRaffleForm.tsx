9e1a6c649d344636ff3d4e4fa92dfeac72f7f0a3
Anthony Urbina <aurbinava002@gmail.com>
2023-03-16 18:24:02 +0000

    installed formik and yup package

--- components/elements/createRaffleForm/createRaffleForm.tsx
+++ components/elements/createRaffleForm/createRaffleForm.tsx
@@ -26,30 +26,24 @@ import {
 import { AddIcon } from '@chakra-ui/icons'
 import { ChangeEvent, useState } from 'react'
 
-export interface CreateRaffleFormProps {
-    title: string
-    description: string
-    image: string
-    altText: string
-    date: string
-    route: string
-    past: boolean
-}
+// export interface CreateRaffleFormProps {
+//     title: string
+//     description: string
+//     image: string
+//     altText: string
+//     date: string
+//     route: string
+//     past: boolean
+// }
 
-export const CreateRaffleForm = ({
-    title,
-    description,
-    image,
-    altText,
-    date,
-    route,
-    past
-}: CreateRaffleFormProps) => {
+export const CreateRaffleForm = () => {
     const [ticketPrice, setTicketPrice] = useState('')
     const [reservePrice, setReservePrice] = useState('')
+    const [err, setErr] = useState('')
 
     const [maxPrecisionTicketErr, setMaxPrecisionTicketErr] = useState(false)
     const [maxPrecisionReserveErr, setMaxPrecisionReserveErr] = useState(false)
+    const CURRENT_DATE_AND_TIME = new Date().toISOString().split(".")[0]
 
 
     const handleTicketPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -58,6 +52,7 @@ export const CreateRaffleForm = ({
         const MAX_DECIMAL_PLACES = 18
         setTicketPrice(value);
         if (parsedValue && value.includes('.') && value.split('.')[1].length > MAX_DECIMAL_PLACES) {
+            setErr('18 decimal points is the maximum precision supported by Ethereum')
             setMaxPrecisionTicketErr(true)
         } else {
             setMaxPrecisionTicketErr(false)
@@ -71,12 +66,17 @@ export const CreateRaffleForm = ({
         const MAX_DECIMAL_PLACES = 18
         setReservePrice(value);
         if (parsedValue && value.includes('.') && value.split('.')[1].length > MAX_DECIMAL_PLACES) {
+            setErr('18 decimal points is the maximum precision supported by Ethereum')
             setMaxPrecisionReserveErr(true)
         } else {
             setMaxPrecisionReserveErr(false)
 
         }
     };
+
+    const displayErrMsg = () => {
+        err && <FormErrorMessage>{ err }</FormErrorMessage>
+    }
 
     return (
         <>
@@ -87,7 +87,7 @@ export const CreateRaffleForm = ({
             </Button>
         </Box>
     </Flex>
-        <FormControl>
+        <FormControl isInvalid={maxPrecisionReserveErr || maxPrecisionTicketErr}>
             <Flex p='1rem' flexWrap='wrap' justify='space-between' rounded='1rem' border='1px' m='.75rem'>
                 <Flex direction='column' align='center'>
                     <FormLabel marginEnd='none' mb='none'>Currency</FormLabel>
@@ -97,32 +97,29 @@ export const CreateRaffleForm = ({
                     </Select>
                 </Flex>
                 <Flex direction='column' align='center'>
-                    <FormControl isInvalid={maxPrecisionTicketErr}>
-                        <FormLabel marginEnd='none' mb='none' textAlign='center'>Ticket Price</FormLabel>
-                        {
-                            maxPrecisionTicketErr ?
-                                <FormErrorMessage mt='none' mb='.25rem' fontSize='9px' textAlign='center'>18 decimal points max</FormErrorMessage>
-                                : <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
-                        }
-                        <Input data-view-field='ticket-price' type='text' w='150px' h='25px' rounded='1rem' borderColor='black' value={ticketPrice} onChange={handleTicketPriceChange}></Input>
-                    </FormControl>
+                    <FormLabel marginEnd='none' mb='none' textAlign='center'>Ticket Price</FormLabel>
+                    <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
+                        <Input data-view-field='ticket-price' type='text' w='150px' h='25px' rounded='1rem' borderColor='black' value={ticketPrice} onChange={handleTicketPriceChange}/>
                 </Flex>
                 <Flex direction='column' align='center' pt='1rem'>
-                    <FormControl isInvalid={maxPrecisionReserveErr}>
                         <FormLabel marginEnd='none' mb='none'>Reserve Price</FormLabel>
-                        {
-                            maxPrecisionReserveErr ?
-                                <FormErrorMessage mt='none' mb='.25rem' fontSize='9px' textAlign='center'>18 decimal points max</FormErrorMessage>
-                                : <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
-                        }
-                        <Input type='number' w='150px' h='25px' rounded='1rem' borderColor='black' value={reservePrice} onChange={handleReservePriceChange}></Input>
-                    </FormControl>
+                        <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
+                        <Input type='text' w='150px' h='25px' rounded='1rem' borderColor='black' value={reservePrice} onChange={handleReservePriceChange}/>
                 </Flex>
                 <Flex direction='column' align='center' pt='1rem'>
                     <FormLabel marginEnd='none' mb='none'>Raffle End Date</FormLabel>
                     <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Raffle minimum</FormHelperText>
-                    <Input type='text' w='150px' h='25px' rounded='1rem' borderColor='black'></Input>
+                    <Input
+                    w='150px'
+                    h='25px'
+                    rounded='1rem'
+                    borderColor='black'
+                    placeholder="Select Date and Time"
+                    size="md"
+                    type="datetime-local"
+                    min={CURRENT_DATE_AND_TIME}/>
                 </Flex>
+                {err && <FormErrorMessage fontSize='12px'>{err}</FormErrorMessage>}
             </Flex>
             <Flex justify='center' pb='.5rem'>
                 <Button bgColor='black' color='white' rounded='1.5rem' fontFamily='Inter' fontWeight='normal' paddingX='8rem'>
