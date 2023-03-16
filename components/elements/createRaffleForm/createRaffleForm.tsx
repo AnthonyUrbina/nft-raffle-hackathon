c0b353c8408bd01cc2916d22dc8179fe2e912a0b
Anthony Urbina <aurbinava002@gmail.com>
2023-03-16 20:04:44 +0000

    regex checks for tprice and rprice complete, ty chatgpt

--- components/elements/createRaffleForm/createRaffleForm.tsx
+++ components/elements/createRaffleForm/createRaffleForm.tsx
@@ -21,9 +21,12 @@ import {
     FormErrorMessage,
     FormHelperText,
     Input,
-    Select
+    Select,
+    VStack
 } from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
+import { useFormik } from "formik";
+import * as yup from "yup"
 import { ChangeEvent, useState } from 'react'
 
 // export interface CreateRaffleFormProps {
@@ -37,96 +40,117 @@ import { ChangeEvent, useState } from 'react'
 // }
 
 export const CreateRaffleForm = () => {
-    const [ticketPrice, setTicketPrice] = useState('')
-    const [reservePrice, setReservePrice] = useState('')
-    const [err, setErr] = useState('')
-
-    const [maxPrecisionTicketErr, setMaxPrecisionTicketErr] = useState(false)
-    const [maxPrecisionReserveErr, setMaxPrecisionReserveErr] = useState(false)
     const CURRENT_DATE_AND_TIME = new Date().toISOString().split(".")[0]
 
+    const formik = useFormik({
+        initialValues: {
+            reservePrice: '0.01',
+            ticketPrice: '0.01'
+        },
+        validationSchema: yup.object({
+            ticketPrice: yup
+                .string()
+                .matches(/^[0-9]+(?:\.[0-9]*)?$/gm, 'Please enter a valid amount')
+                .matches(/^(\d{1,18}(\.\d{0,18})?)?$/, '18 decimal places max')
+                .matches(/^(?!0\d)(?:\d{1,14}(?:\.\d{0,18})?|100000000000000(?:\.0{1,18})?)$/, 'The amount cannot exceed 100,000,000,000,000'),
+            reservePrice: yup
+                .string()
+                .matches(/^[0-9]+(?:\.[0-9]*)?$/gm, 'Please enter a valid amount')
+                .matches(/^(?!0\d)(?:\d{1,14}(?:\.\d{0,18})?|100000000000000(?:\.0{1,18})?)$/, 'The amount cannot exceed 100,000,000,000,000')
+                .matches(/^(\d{1,18}(\.\d{0,18})?)?$/, '18 decimal places max'),
+        }),
+        onSubmit: (values) => {
+            console.log(values);
+        },
+    });
 
-    const handleTicketPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.target;
-        const parsedValue = parseFloat(value);
-        const MAX_DECIMAL_PLACES = 18
-        setTicketPrice(value);
-        if (parsedValue && value.includes('.') && value.split('.')[1].length > MAX_DECIMAL_PLACES) {
-            setErr('18 decimal points is the maximum precision supported by Ethereum')
-            setMaxPrecisionTicketErr(true)
-        } else {
-            setMaxPrecisionTicketErr(false)
 
-        }
-    };
-
-    const handleReservePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.target;
-        const parsedValue = parseFloat(value);
-        const MAX_DECIMAL_PLACES = 18
-        setReservePrice(value);
-        if (parsedValue && value.includes('.') && value.split('.')[1].length > MAX_DECIMAL_PLACES) {
-            setErr('18 decimal points is the maximum precision supported by Ethereum')
-            setMaxPrecisionReserveErr(true)
-        } else {
-            setMaxPrecisionReserveErr(false)
-
-        }
-    };
-
-    const displayErrMsg = () => {
-        err && <FormErrorMessage>{ err }</FormErrorMessage>
-    }
+    const { ticketPrice, reservePrice } = formik.values
+    const { handleChange, errors } = formik
 
     return (
         <>
-    <Flex border='1px' rounded='1rem' align='center' justify='center' boxSize={353} m='0 auto' bgColor='#D9D9D9'>
-        <Box>
-            <Button leftIcon={<AddIcon mb='.5rem' boxSize={9}/>} display='flex' flexDir='column' bgColor='#D9D9D9'>
-                Select NFT
-            </Button>
-        </Box>
-    </Flex>
-        <FormControl isInvalid={maxPrecisionReserveErr || maxPrecisionTicketErr}>
-            <Flex p='1rem' flexWrap='wrap' justify='space-between' rounded='1rem' border='1px' m='.75rem'>
-                <Flex direction='column' align='center'>
-                    <FormLabel marginEnd='none' mb='none'>Currency</FormLabel>
-                    <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>The token used for buying tickets</FormHelperText>
-                    <Select placeholder='MATIC' w='150px' h='25px' textAlign='center' rounded='1rem' borderColor='black'>
-                        <option value="APE COIN">APE COIN</option>
-                    </Select>
-                </Flex>
-                <Flex direction='column' align='center'>
-                    <FormLabel marginEnd='none' mb='none' textAlign='center'>Ticket Price</FormLabel>
-                    <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
-                        <Input data-view-field='ticket-price' type='text' w='150px' h='25px' rounded='1rem' borderColor='black' value={ticketPrice} onChange={handleTicketPriceChange}/>
-                </Flex>
-                <Flex direction='column' align='center' pt='1rem'>
-                        <FormLabel marginEnd='none' mb='none'>Reserve Price</FormLabel>
-                        <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
-                        <Input type='text' w='150px' h='25px' rounded='1rem' borderColor='black' value={reservePrice} onChange={handleReservePriceChange}/>
-                </Flex>
-                <Flex direction='column' align='center' pt='1rem'>
-                    <FormLabel marginEnd='none' mb='none'>Raffle End Date</FormLabel>
-                    <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Raffle minimum</FormHelperText>
-                    <Input
-                    w='150px'
-                    h='25px'
-                    rounded='1rem'
-                    borderColor='black'
-                    placeholder="Select Date and Time"
-                    size="md"
-                    type="datetime-local"
-                    min={CURRENT_DATE_AND_TIME}/>
-                </Flex>
-                {err && <FormErrorMessage fontSize='12px'>{err}</FormErrorMessage>}
+            <Flex border='1px' rounded='1rem' align='center' justify='center' boxSize={353} m='0 auto' bgColor='#D9D9D9'>
+                <Box>
+                    <Button leftIcon={<AddIcon mb='.5rem' boxSize={9}/>} display='flex' flexDir='column' bgColor='#D9D9D9'>
+                        Select NFT
+                    </Button>
+                </Box>
             </Flex>
-            <Flex justify='center' pb='.5rem'>
-                <Button bgColor='black' color='white' rounded='1.5rem' fontFamily='Inter' fontWeight='normal' paddingX='8rem'>
-                Create Raffle
-                </Button>
-            </Flex>
-    </FormControl>
+            <FormControl>
+                <Flex p='1rem' flexWrap='wrap' justify='space-between' rounded='1rem' border='1px' m='.75rem'>
+                    <Flex direction='column' align='center'>
+                        <FormControl>
+                            <FormLabel htmlFor='currency' marginEnd='none' mb='none'>Currency</FormLabel>
+                            <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>The token used for buying tickets</FormHelperText>
+                            <Select
+                                id='currency'
+                                placeholder='MATIC'
+                                w='150px' h='25px'
+                                textAlign='center'
+                                rounded='1rem'
+                                borderColor='black'>
+                                <option value="APE COIN">APE COIN</option>
+                            </Select>
+                        </FormControl>
+                    </Flex>
+                    <Flex direction='column' align='center'>
+                        <FormControl isInvalid={!!errors.ticketPrice}>
+                            <FormLabel htmlFor='ticket-price' marginEnd='none' mb='none' textAlign='center'>Ticket Price</FormLabel>
+                            {
+                                errors.ticketPrice ? <FormErrorMessage mt='none' mb='.25rem' fontSize='9px' textAlign='center'>{errors.ticketPrice}</FormErrorMessage>
+                                    : <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
+
+                            }
+                            <Input
+                                id='ticket-price'
+                                name='ticketPrice'
+                                type='text'
+                                w='150px'
+                                h='25px'
+                                rounded='1rem'
+                                borderColor='black'
+                                value={ticketPrice}
+                                onChange={handleChange}/>
+                        </FormControl>
+                    </Flex>
+                    <Flex direction='column' align='center' pt='1rem'>
+                        <FormControl>
+                            <FormLabel marginEnd='none' mb='none'>Reserve Price</FormLabel>
+                            <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Choose your price per ticket</FormHelperText>
+                            <Input
+                                id="reserve-price"
+                                name='reservePrice'
+                                w='150px' h='25px'
+                                rounded='1rem'
+                                borderColor='black'
+                                value={reservePrice}
+                                onChange={handleChange}/>
+                        </FormControl>
+                    </Flex>
+                    <Flex direction='column' align='center' pt='1rem'>
+                        <FormControl>
+                            <FormLabel htmlFor='calendar' marginEnd='none' mb='none'>Raffle End Date</FormLabel>
+                            <FormHelperText mt='none' mb='.25rem' fontSize='9px' textAlign='center'>Raffle minimum</FormHelperText>
+                            <Input
+                                id="calendar"
+                                w='150px'
+                                h='25px'
+                                rounded='1rem'
+                                borderColor='black'
+                                placeholder="Select Date and Time"
+                                size="md"
+                                type="datetime-local"
+                                min={CURRENT_DATE_AND_TIME}/>
+                        </FormControl>
+                    </Flex>
+                </Flex>
+                <Flex justify='center' pb='.5rem'>
+                    <Button bgColor='black' color='white' rounded='1.5rem' fontFamily='Inter' fontWeight='normal' paddingX='8rem'>
+                    Create Raffle
+                    </Button>
+                </Flex>
+        </FormControl>
     </>
     )
 }
