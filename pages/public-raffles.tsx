a7c21c3fa3da205858bcb38b94f83ebc3215192d
Anthony Urbina <aurbinava002@gmail.com>
2023-03-17 23:26:02 +0000

    removed react datepicker package

--- pages/public-raffles.tsx
+++ pages/public-raffles.tsx
@@ -1,24 +1,9 @@
 import { memo } from 'react'
-// import { ErrorBoundary, Meta } from '../components/elements'
 import { PublicRaffles } from '../components/pages'
-// import {
-//     ERROR_FALLBACK,
-//     BUBBLE_BOAT_MIAMI_TITLE,
-//     BASE_URL,
-//     BUBBLE_BOAT_MIAMI,
-//     BUBBLE_BOAT_MIAMI_IMAGE,
-// } from '../constants'
 
 const PublicRafflesContainer = () => {
     return (
-        // <ErrorBoundary fallback={ERROR_FALLBACK}>
-        // {/* <Meta
-        //         title={CREATE_RAFFLE}
-        //         url={`${BASE_URL}${CREATE_RAFFLE}`}
-        //         image={CREATE_RAFFLE_IMAGE}
-        //     /> */}
         < PublicRaffles />
-        // {/* </ErrorBoundary> */ }
     )
 }
 
