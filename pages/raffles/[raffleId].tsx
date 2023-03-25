5a596044d2799d5a8cbfd191667183d3aaf72ae6
Anthony Urbina <aurbinava002@gmail.com>
2023-03-25 02:07:26 +0000

    pasted raffleId.tsx

--- pages/raffles/[raffleId].tsx
+++ pages/raffles/[raffleId].tsx
@@ -1,18 +1,35 @@
 import { memo } from 'react'
 import { RaffleDetails } from '../../components/pages'
 import { useRouter } from 'next/router'
-import { getServerSideProps } from '../create-raffle'
+import { GetServerSideProps } from 'next'
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../../firebase';
+import type { DocumentData } from '@firebase/firestore-types';
 
 
 
 const RaffleDetailsContainer = () => {
     const router = useRouter()
-    let { id } = router.query
-    id = id!.toString()
+    let { raffleId } = router.query
+    console.log('raffleId', router.query)
+    raffleId = raffleId!.toString()
 
     return (
-        < RaffleDetails raffleId={id} />
+        < RaffleDetails />
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    let { raffleId } = context.query
+    console.log('raffleId', raffleId)
+    const querySnapshot = await getDocs(collection(db, "raffles"));
+    const documents = querySnapshot.docs.map(doc => doc.data());
+    console.log('documents', documents)
+    const raffle = documents.filter(raffle => { console.log('raffle', raffle); return raffle.raffleId === Number(raffleId) })
+    console.log('raffle', raffle)
+    return {
+        props: { raffle: raffle[0] ?? {} }
+    }
 }
 
 RaffleDetailsContainer.displayName = 'Create Raffle Container'
